79e771c52e597c0714778c2bf5d8a8daeba77b9f
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2020-03-15 22:20:22 +0800

    installed packages
    - react router dom
    - react-ga

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,34 @@ import './App.css';
 import {Client as Styletron} from 'styletron-engine-atomic';
 import {Provider as StyletronProvider} from 'styletron-react';
 import {LightTheme, BaseProvider, styled} from 'baseui';
+import { Router, Route, Switch } from "react-router-dom"
+import createHistory from "history/createBrowserHistory"
+import ReactGA from "react-ga"
 
+const history = createHistory()
+history.listen((location, action) => {
+	ReactGA.set({ page: location.pathname })
+	ReactGA.pageview(location.pathname)
+})
 
 const engine = new Styletron();
-const Centered = styled('div', {
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '100%',
-});
 
+const Routes = () => {
+	React.useEffect(() => {})
+
+	return (
+			<Router history={history}>
+				<Switch>
+				</Switch>
+			</Router>
+	)
+}
 
 export default function App() {
   return (
     <StyletronProvider value={engine}>
       <BaseProvider theme={LightTheme}>
-        <Centered>
-        </Centered>
+        <Routes />
       </BaseProvider>
     </StyletronProvider>
   );
