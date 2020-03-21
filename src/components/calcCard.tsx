b125bda42d8585438eae131366026241781f449b
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2020-03-21 22:26:03 +0800

    push

--- src/components/calcCard.tsx
+++ src/components/calcCard.tsx
@@ -1,76 +1,87 @@
-
-import React from 'react'
-import { useStyletron } from "styletron-react"
-import {Input} from 'baseui/input';
+import React from "react";
+import { useStyletron } from "styletron-react";
+import { Input } from "baseui/input";
 import { Button } from "baseui/button";
-import "../styles.scss"
-import { totalmem } from 'os';
+import "../styles.scss";
+import { totalmem } from "os";
 
 export const CalcCard = () => {
+  const [css] = useStyletron();
 
-    const [css] = useStyletron()
-    
-    const topDiv: string  =css({
-        display: "flex",
-        justifyContent: "center",
-        textAlign: "center",
-    })
+  const topDiv: string = css({
+    display: "flex",
+    justifyContent: "center",
+    textAlign: "center"
+  });
 
-    const cardParent: string =css({
-        width: "550px",
-        height: "600px",
-        marginTop: "180px",
-        justifyContent: "center",
-        background: "white",
-        borderRadius: "10px",
-        padding: "30px",
-        boxShadow: "0px 3px 6px #00000029",
-    })
+  const cardParent: string = css({
+    width: "550px",
+    height: "600px",
+    marginTop: "180px",
+    justifyContent: "center",
+    background: "white",
+    borderRadius: "10px",
+    padding: "30px",
+    boxShadow: "0px 3px 6px #00000029"
+  });
 
-    const titleParent: string =css({
-        fontFamily: "sheep",
-        fontSize: "35px",
-        color: "#4B9CD3",
-    })
+  const titleParent: string = css({
+    fontFamily: "sheep",
+    fontSize: "35px",
+    color: "#4B9CD3"
+  });
 
-    const titleH2: string =css({
-        fontSize: "35px",
-        color: "#4B9CD3",
-    })
+  const titleH2: string = css({
+    fontSize: "35px",
+    color: "#4B9CD3"
+  });
 
-    
-    
-    return (
-        <div className={topDiv}>
-            <div className={cardParent}>
-            <h1 className={titleParent}>Sneaker Profit</h1>
-            <div>            
-                <React.Fragment>
-                    <Input placeholder="Cost"/>
-                    <br/>
-                    <Input placeholder="Resell"/>
-                    <br/>
-                    <Input placeholder="Shipping"/>
-                    <br/>
-                    <Input placeholder="Fees"/>
-                </React.Fragment>
-                <br/>
-                <Button overrides={{
-                    BaseButton: {
-                        style: {
-                            backgroundColor:"#4B9CD3",
-                        }
-                    }
-                }} 
-                onClick={() => alert("Your profits will be shown soon...")}>Calculate</Button>
-                <h2 className={titleH2}>Profit:</h2>
-            </div>
+  let cost: number = 0;
+  let resell: number = 0;
+  let shipping: number = 0;
+  let fees: number = 0;
+
+  function handleCalc(
+    cost: number,
+    resell: number,
+    shipping: number,
+    fees: number
+  ) {
+    return resell - cost - (shipping - fees);
+  }
+
+  return (
+    <div className={topDiv}>
+      <div className={cardParent}>
+        <h1 className={titleParent}>Sneaker Profit</h1>
+        <div>
+          <React.Fragment>
+            <form>
+              <Input placeholder="Cost" value={cost} />
+              <br />
+              <Input placeholder="Resell" value={resell} />
+              <br />
+              <Input placeholder="Shipping" value={shipping} />
+              <br />
+              <Input placeholder="Fees" value={fees} />
+            </form>
+          </React.Fragment>
+          <br />
+          <Button
+            overrides={{
+              BaseButton: {
+                style: {
+                  backgroundColor: "#4B9CD3"
+                }
+              }
+            }}
+            onClick={() => alert("Your profits will be shown soon...")}
+          >
+            Calculate
+          </Button>
+          <h2 className={titleH2}>Profit:</h2>
         </div>
-        </div>
-
-    )
-}
-
-
-
-
+      </div>
+    </div>
+  );
+};
---
+++ src/unstated/userStore.tsx
@@ -0,0 +1,4 @@
+import * as React from "react";
+
+import { useState } from "react";
+import { createContainer } from "unstated-next";
